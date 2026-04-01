61897db671f0235dac070b81474cc432785fa89b
nagasrivatti <151708465+nagasrivatti@users.noreply.github.com>
2026-04-01 11:29:35 +0530

    2.AND_XOR_operations_with_127.c

---
+++ AND_XOR_operations_with_127.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void main() {
+	char str[] = "Hello World";
+	int length, i;
+	char ch;
+	length = strlen(str);
+	printf("Original String is: %s\n\n", str);
+	printf("Encrypted String with AND:\n");
+	for(i = 0; i < length; i++) {
+		ch = str[i] & 127;
+		printf("%c", ch);
+	}
+	printf("\nEncrypted String with XOR:\n");
+	for(i = 0; i < length; i++) {
+		ch = str[i] ^ 127;
+		printf("%c", ch);
+	}
+}
+o/p:
+Original String is: Hello World
+Encrypted String with AND:
+Hello World
+Encrypted String with XOR:7_c
